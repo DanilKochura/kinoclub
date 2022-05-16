78cb3ab55b34873ec6b04c96d3919acc2c0e6ec9
Danil Kochura <kochura2017@yandex.ru>
2022-05-17 01:45:17 +0300

    Delete some shit

--- bd.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$conn = new mysqli("localhost", "root", "admin", "kino");
-			if($conn->connect_error){
-			die("Ошибка: " . $conn->connect_error);
-			}
-	if($conn->connect_error) die("ERROROROR");
-?>
