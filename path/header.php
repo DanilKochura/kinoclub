9359edfaf3d0d4b22c80e274d219a6c3ac946fa4
Danil Kochura <kochura2017@yandex.ru>
2022-05-20 21:10:15 +0300

    Fix links in header

--- path/header.php
+++ path/header.php
@@ -19,8 +19,8 @@
 	        </a>
 
 	        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-	          <li><a href="../imdibil" class="nav-link px-2 text-secondary">Главная</a></li>
-	          <li><a href="../experts/index.php" class="nav-link px-2 text-white">Наши Эксперты</a></li>
+	          <li><a href="index.php" class="nav-link px-2 text-secondary">Главная</a></li>
+	          <li><a href="#" class="nav-link px-2 text-white">Наши Эксперты</a></li>
 	          <li><a href="news.php" class="nav-link px-2 text-white">Новости</a></li>
 	          <li><a href="#" class="nav-link px-2 text-white">Викторина</a></li>
 	        </ul>
