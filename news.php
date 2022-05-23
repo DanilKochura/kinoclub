0e2725df77fa311efc49a4ff8e40a9effcce5ac4
Danil Kochura <kochura2017@yandex.ru>
2022-05-23 23:00:38 +0300

    Added news layout

--- news.php
+++ news.php
@@ -3,3 +3,94 @@
 	require 'path/header.php';
 
 ?>
+<div class="container forum-card">
+  		<div class="row">
+  				<div class="col-sm-2 thirds">
+  					<a href="https://www.kinopoisk.ru/film/81691/">	<img src="image/adam.jpg"class="img-fluid rounded"></a>
+  				</div>
+  				<div class="col-sm-2 thirds">
+  					<p class="name">Адамовы яблоки</p>
+		  			<div class="original">Adams æbler</div>
+		  			<div class="year">Год: 2005</div>
+		  			<div class="type">Жанр: драма, комедия, криминал</div>
+		  				<div class="director">Режиссер: Андерс Томас Йенсен</div>
+		  				<div class="time">Длительность: 91мин.</div>
+              <table class="table-rate text-center" style="height: 100px;">
+              	<tbody>
+              		<tr>
+              			<td><img src="image/imdb.png" class="res_logo"></td>
+              			<td class="rate-ch">7.6</td>
+              		</tr>
+              		<tr>
+              			<th><img src="image/kp.png" class="res_logo"></th>
+              			<td class="rate-ch">7.7</td>
+              		</tr>
+              	</tbody>
+              </table>
+
+  				</div>
+  				<div class="col-sm-2 thirds">
+  					<a href="https://www.kinopoisk.ru/film/779602/"><img src="image/who.jpg"class="img-fluid rounded"></a>
+  				</div>
+  				<div class="col-sm-2 thirds">
+  					<p class="name">Кто я</p>
+		  			<div class="original">Who Am I - Kein System ist sicher</div>
+		  			<div class="year">Год: 2014</div>
+		  			<div class="type">Жанр: фантастика, триллер, драма, криминал</div>
+		  				<div class="director">Режиссер:Баран бо Одар</div>
+		  				<div class="time">Длительность: 102 мин.</div>
+              <table class="table-rate text-center" style="height: 100px;">
+              	<tbody>
+              		<tr>
+              			<td><img src="image/imdb.png" class="res_logo"></td>
+              			<td class="rate-ch">7.4</td>
+              		</tr>
+              		<tr>
+              			<th><img src="image/kp.png" class="res_logo"></th>
+              			<td class="rate-ch">7.5</td>
+              		</tr>
+              	</tbody>
+              </table>
+              	</div>
+  				<div class="col-sm-2 thirds">
+  							<a href="https://www.kinopoisk.ru/film/818981/">	<img src="image/real.jpg"class="img-fluid rounded"></a>
+  				</div>
+  				<div class="col-sm-2 thirds">
+  					<p class="name">Реальные упыри</p>
+		  			<div class="original">What We Do in the Shadows</div>
+		  			<div class="year">Год: 2014</div>
+		  			<div class="type">Жанр: комедия, ужасы</div>
+		  				<div class="director">Режиссер: Джемейн Клемент, Тайка Вайтити</div>
+		  				<div class="time">Длительность: 85 мин.</div>
+              <table class="table-rate text-center" style="height: 100px;">
+              	<tbody>
+              		<tr>
+              			<td><img src="image/imdb.png" class="res_logo"></td>
+              			<td class="rate-ch">7.4</td>
+              		</tr>
+              		<tr>
+              			<th><img src="image/kp.png" class="res_logo"></th>
+              			<td class="rate-ch">7.6</td>
+              		</tr>
+              	</tbody>
+              </table>
+
+  				</div>
+  				<div class="row">
+  					<div class="col-sm-4">
+  						Нео-нацист, приговорённый к общественным работам в церкви, сталкивается с крайне праведным священником.
+  					</div>
+  						<div class="col-sm-4">
+  						Бенджамин – молодой компьютерный гений. С детства он мечтает стать супергероем из комиксов и покорить мир. Но в реальном мире он - никто. Его жизнь неожиданно меняется, когда он встречает свою полную противоположность – харизматичного Макса. Не желая жить в жёстких рамках системы, они совершают череду дерзких кибер-преступлений. Восстав против равнодушного общества, они становятся кумирами для целого поколения. Теперь Бенджамин - не просто супергерой, он – самый разыскиваемый хакер в мире. Но что его ждёт после погружения в альтернативную реальность? Кто же он на самом деле?
+  					</div>
+  						<div class="col-sm-4">
+  						История жизни Виаго, Дикона и Владислава — трёх соседей и по совместительству бессмертных вампиров, которые всего лишь пытаются выжить в современном мире, где есть арендная плата, фейсконтроль в ночных клубах, губительный солнечный свет и другие неприятности.
+  					</div>
+  				</div>
+  	</div>
+
+
+ <script src="scripts\bootstrap.bundle.min.js"></script>
+ <script src="scripts\main.js"></script>
+  </body>
+</html>
