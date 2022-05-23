e1f2feb5b88218a6f9e990d4f53a45243d2f61a5
Danil Kochura <kochura2017@yandex.ru>
2022-05-24 01:17:52 +0300

    Added a function for sql errors catching

--- config/bd.php
+++ config/bd.php
@@ -4,4 +4,12 @@ $conn = new mysqli("localhost", "root", "admin", "kino");
 			die("Ошибка: " . $conn->connect_error);
 			}
 	if($conn->connect_error) die("ERROROROR");
+function Query_try($connection, $query) //запрос к бд и прерывание в случае ошибки
+{
+	if(!($result = mysqli_query($connection, $query)))
+	{
+		die("Query error");
+	}
+	return $result;
+}
 ?>
