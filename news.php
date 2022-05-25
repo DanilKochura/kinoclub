abe1ae1523fe92c22ef3473b7ad25b63a6c0f429
Danil Kochura <kochura2017@yandex.ru>
2022-05-25 19:16:06 +0300

    Finished work with generating user's movie trios

--- news.php
+++ news.php
@@ -1,96 +1,64 @@
 <?php 
 	session_start();
 	require 'path/header.php';
-
+	require 'model/GetBase.php';
+	$thirds = GetAllThirds();
+	//debug($thirds);
+	$j=0;
+	foreach ($thirds as $third):
+		if($j==3){$j=0;}
 ?>
-<div class="container forum-card">
+	<div class="container forum-card">
   		<div class="row">
-  				<div class="col-sm-2 thirds">
-  					<a href="https://www.kinopoisk.ru/film/81691/">	<img src="image/adam.jpg"class="img-fluid rounded"></a>
-  				</div>
-  				<div class="col-sm-2 thirds">
-  					<p class="name">Адамовы яблоки</p>
-		  			<div class="original">Adams æbler</div>
-		  			<div class="year">Год: 2005</div>
-		  			<div class="type">Жанр: драма, комедия, криминал</div>
-		  				<div class="director">Режиссер: Андерс Томас Йенсен</div>
-		  				<div class="time">Длительность: 91мин.</div>
-              <table class="table-rate text-center" style="height: 100px;">
-              	<tbody>
-              		<tr>
-              			<td><img src="image/imdb.png" class="res_logo"></td>
-              			<td class="rate-ch">7.6</td>
-              		</tr>
-              		<tr>
-              			<th><img src="image/kp.png" class="res_logo"></th>
-              			<td class="rate-ch">7.7</td>
-              		</tr>
-              	</tbody>
-              </table>
-
-  				</div>
-  				<div class="col-sm-2 thirds">
-  					<a href="https://www.kinopoisk.ru/film/779602/"><img src="image/who.jpg"class="img-fluid rounded"></a>
-  				</div>
-  				<div class="col-sm-2 thirds">
-  					<p class="name">Кто я</p>
-		  			<div class="original">Who Am I - Kein System ist sicher</div>
-		  			<div class="year">Год: 2014</div>
-		  			<div class="type">Жанр: фантастика, триллер, драма, криминал</div>
-		  				<div class="director">Режиссер:Баран бо Одар</div>
-		  				<div class="time">Длительность: 102 мин.</div>
-              <table class="table-rate text-center" style="height: 100px;">
-              	<tbody>
-              		<tr>
-              			<td><img src="image/imdb.png" class="res_logo"></td>
-              			<td class="rate-ch">7.4</td>
-              		</tr>
-              		<tr>
-              			<th><img src="image/kp.png" class="res_logo"></th>
-              			<td class="rate-ch">7.5</td>
-              		</tr>
-              	</tbody>
-              </table>
-              	</div>
-  				<div class="col-sm-2 thirds">
-  							<a href="https://www.kinopoisk.ru/film/818981/">	<img src="image/real.jpg"class="img-fluid rounded"></a>
-  				</div>
-  				<div class="col-sm-2 thirds">
-  					<p class="name">Реальные упыри</p>
-		  			<div class="original">What We Do in the Shadows</div>
-		  			<div class="year">Год: 2014</div>
-		  			<div class="type">Жанр: комедия, ужасы</div>
-		  				<div class="director">Режиссер: Джемейн Клемент, Тайка Вайтити</div>
-		  				<div class="time">Длительность: 85 мин.</div>
-              <table class="table-rate text-center" style="height: 100px;">
-              	<tbody>
-              		<tr>
-              			<td><img src="image/imdb.png" class="res_logo"></td>
-              			<td class="rate-ch">7.4</td>
-              		</tr>
-              		<tr>
-              			<th><img src="image/kp.png" class="res_logo"></th>
-              			<td class="rate-ch">7.6</td>
-              		</tr>
-              	</tbody>
-              </table>
-
-  				</div>
-  				<div class="row">
-  					<div class="col-sm-4">
-  						Нео-нацист, приговорённый к общественным работам в церкви, сталкивается с крайне праведным священником.
-  					</div>
-  						<div class="col-sm-4">
-  						Бенджамин – молодой компьютерный гений. С детства он мечтает стать супергероем из комиксов и покорить мир. Но в реальном мире он - никто. Его жизнь неожиданно меняется, когда он встречает свою полную противоположность – харизматичного Макса. Не желая жить в жёстких рамках системы, они совершают череду дерзких кибер-преступлений. Восстав против равнодушного общества, они становятся кумирами для целого поколения. Теперь Бенджамин - не просто супергерой, он – самый разыскиваемый хакер в мире. Но что его ждёт после погружения в альтернативную реальность? Кто же он на самом деле?
-  					</div>
-  						<div class="col-sm-4">
-  						История жизни Виаго, Дикона и Владислава — трёх соседей и по совместительству бессмертных вампиров, которые всего лишь пытаются выжить в современном мире, где есть арендная плата, фейсконтроль в ночных клубах, губительный солнечный свет и другие неприятности.
-  					</div>
-  				</div>
+  			<div class="col-sm-12 text-center">
+  				<div class="name">Добавлена тройка фильмов от пользователя <?=$third[$j]['name']?> </div>
+  			</div>
+  			<div class="selected" style="display: none;"><?=$third[$j]['selected']?></div>
+  		</div>
   	</div>
+    <div class="container forum-card">
+  		<div class="row">
+  			<?php foreach($third as $film): ?>
+
+  			<div class="col-sm-4 thirds" id="id-<?=$film['id_m']?>">
+  				<div class="row">
+  					<div class="col-sm-6">
+  						<a href="<?=$film['url']?>">	<img src="<?=$film['poster']?>"class="img-fluid rounded" id="IM"></a>
+  					</div>
+  					<div class="col-sm-6">
+  						<h5 class="name"><?=$film['name_m']?></h5>
+		  				<div class="original"><?=$film['original']?></div>
+		  				<div class="year">Год: <?=$film['year_of_cr']?></div>
+		  				<div class="type">Жанр: драма, комедия, криминал</div>
+		  				<div class="director">Режиссер: <?=$film['name_d']?></div>
+		  				<div class="time">Длительность: <?=$film['duration']?>мин.</div>
+		                <table class="table-rate text-center" style="height: 100px;">
+		              	    <tbody>
+		              			<tr>
+		              				<td><img src="image/imdb.png" class="res_logo"></td>
+		              				<td class="rate-ch"><?=$film['rating']?></td>
+		              			</tr>
+		              			<tr>
+		              				<th><img src="image/kp.png" class="res_logo"></th>
+		              				<td class="rate-ch"><?=$film['rating_kp']?></td>
+		              			</tr>
+		              		</tbody>
+		                </table>
+  					</div>
+  				</div>
+  				<div class="row description">
+  					<div class="col-sm-12 thirds"><?=$film['description']?></div>
+  				</div>
+  			</div>
+  		<?php endforeach; ?>
+  			
+  		</div>
+  	</div>
+  <?php ++$j; endforeach; ?>
 
 
  <script src="scripts\bootstrap.bundle.min.js"></script>
  <script src="scripts\main.js"></script>
   </body>
 </html>
+
