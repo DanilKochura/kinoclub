1ba0787f139a175d261d95f05c1918cbeb57f547
Danil Kochura <kochura2017@yandex.ru>
2022-05-25 19:18:17 +0300

    Added the script for searching and framing communty selected film

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,21 @@ function rateCheck(el)
 		else if(el.textContent>=5.0){el.classList.add("grey-zone");}
 			else el.classList.add("red-zone");;
 }
+
+const Select = document.querySelectorAll('.selected');
+Select.forEach(element => {
+  console.log(element.textContent);
+  SelectCheck(element.textContent);
+});
+
+// Access the first element in the NodeList
+Select[0];
+function SelectCheck(el)
+{
+	let id = "#id-"+el
+	console.log(id);
+	const RateC = document.querySelector(id);
+	RateC.classList.add("selected");
+
+}
+RateC[0];
