cdb4b90ac3e3f7ee9573b79b3f359fee332a8d12
Danil Kochura <kochura2017@yandex.ru>
2022-06-13 16:56:59 +0300

    Fixed news selected film borders

--- scripts/main.js
+++ scripts/main.js
@@ -11,23 +11,25 @@ function rateCheck(el)
 {
 	if(el.textContent>= 7.0){el.classList.add("green-zone");}
 		else if(el.textContent>=5.0){el.classList.add("grey-zone");}
-			else el.classList.add("red-zone");;
+			else el.classList.add("red-zone");
 }
 
 const Select = document.querySelectorAll('.selected');
 Select.forEach(element => {
   console.log(element.textContent);
-  SelectCheck(element.textContent);
+  SelectCheck(element);
 });
 
 // Access the first element in the NodeList
 Select[0];
 function SelectCheck(el)
 {
-	let id = "#id-"+el
+    if(el.innerHTML === ""){console.log("Not selected");}
+    else {
+	let id = "#id-"+el.textContent;
 	console.log(id);
 	const RateC = document.querySelector(id);
-	RateC.classList.add("selected");
+	RateC.classList.add("selected"); }
 
 }
 RateC[0];
