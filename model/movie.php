d5de4f0a606568bc21d0341eceb050173d5f4a56
Danil Kochura <kochura2017@yandex.ru>
2022-08-20 20:27:27 +0300

    Страница и класс для фильмов (пока для отладки и проверки)

---
+++ model/movie.php
@@ -0,0 +1,40 @@
+<?php
+require PATH.'/config/bd.php';
+
+class Movie extends DB
+{
+    public string $name;
+    public string $original;
+    public string $poster;
+    public string $description;
+    public int $year;
+    public float $rating;
+    public float $imdb;
+    public int $duration;
+    public string $director;
+    public string $url;
+    public function __construct($id)
+    {
+        parent::__construct();
+        $q = "SELECT * from movie join director on director=id_d where id_m='$id'";
+//        echo $q;
+        $r = $this->Query_try($q);
+//        print_r($r);
+        $res = mysqli_fetch_assoc($r);
+//        print_r($res);
+        $this->name = $res['name_m'];
+        $this->original = $res['original'];
+        $this->poster = $res['poster'];
+        $this->year = $res['year_of_cr'];
+        $this->description = $res['description'];
+        $this->rating = $res['rating_kp'];
+        $this->imdb = $res['rating'];
+        $this->duration = $res['duration'];
+        $this->url = $res['url'];
+        $this->director = $res['name_d'];
+    }
+    public function debug()
+    {
+        echo $this->poster.PHP_EOL;
+    }
+}
--- pages/movies.php
+++ pages/movies.php
@@ -1,2 +1,24 @@
 <?php
 
+require PATH.'/model/movie.php';
+//echo '2';
+$m = new Movie((int)$_GET[0]);
+//echo '2';
+
+
+
+?>
+
+<div class="row">
+    <div class="col-sm-2">
+        <img src="<?=$m->poster?>" alt="" class="img-fluid">
+    </div>
+    <div class="col-sm-5">
+        dfdf
+        <div class="row"><?=$m->name?></div>
+        <div class="row"><?=$m->original?></div>
+        <div class="row"><?=$m->director?></div>
+        <div class="row"><?=$m->year?></div>
+        <div class="row"><?=$m->rating?></div>
+    </div>
+</div>
