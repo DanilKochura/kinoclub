194674350ebc036cd031092377bb472ec4b7cb43
Danil Kochura <kochura2017@yandex.ru>
2022-09-14 23:40:17 +0300

    Дебаг добавления пользовательских оценок

--- scripts/ajax/addrate.php
+++ scripts/ajax/addrate.php
@@ -2,7 +2,7 @@
 //session_start();
 //ini_set('display_errors', 1);
 error_reporting(E_ALL);
-
+session_start();
 include '../../model/PostBase.php';
 
 file_put_contents(__DIR__.'/0.txt', print_r($_POST, 1));
@@ -31,6 +31,15 @@ $base = new PostBase();
 $rate = $_POST['rating'];
 $meet = $_POST['movie'];
 $id = $_SESSION['user']['id'];
+if(!$id)
+{
+    $arr = array(
+        'state'=>0,
+        'text' =>'Пользователь не найден'
+    );
+    echo json_encode($arr);
+    exit;
+}
 
 $base->AddRate($rate, $meet, $id);
 $arr = array(
